747c976d2055ea6ffaa3f8f36dd9e41f788bbf86
HarshaVardhanRao <Hvijapuram@gmail.com>
2025-03-09 11:17:22 +0530

    Update ALLOWED_HOSTS to include localhost for local development

--- gmocs2k25/gmocs2k25/settings.py
+++ gmocs2k25/gmocs2k25/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = 'django-insecure-kl2hz4$f6f@$_c3f+nd%&z^-2(c&zhm$=4*nhmsq3xfc^y!5so
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['gmocs.pythonanywhere.com']
+ALLOWED_HOSTS = ['gmocs.pythonanywhere.com','127.0.0.1']
 
 
 # Application definition
