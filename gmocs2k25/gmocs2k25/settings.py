6069386bedb663fb2dcdbf2df6e4c9013e249370
HarshaVardhanRao <Hvijapuram@gmail.com>
2025-03-09 12:13:28 +0530

    Update ALLOWED_HOSTS comment to clarify production verification

--- gmocs2k25/gmocs2k25/settings.py
+++ gmocs2k25/gmocs2k25/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-kl2hz4$f6f@$_c3f+nd%&z^-2(c&zhm$=4*nhmsq3xfc^y!5so
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
-
+#Verified in production
 ALLOWED_HOSTS = ['gmocs.pythonanywhere.com','127.0.0.1','localhost']
 
 
