51c5f41034536c61ef8df5ea5967c00bcad87fab
V Mahesh Kumar <maheshkumarvmk0@gmail.com>
2025-03-12 19:47:36 +0530

    Set DEBUG to False for production environment

--- gmocs2k25/gmocs2k25/settings.py
+++ gmocs2k25/gmocs2k25/settings.py
@@ -25,7 +25,7 @@ print(BASE_DIR)
 SECRET_KEY = 'django-insecure-kl2hz4$f6f@$_c3f+nd%&z^-2(c&zhm$=4*nhmsq3xfc^y!5so'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 #Verified in production
 ALLOWED_HOSTS = ['98.70.72.145','www.gmocs.live', 'gmocs.live', 'gmocs.pythonanywhere.com','127.0.0.1','localhost']
 
